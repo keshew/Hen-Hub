c73df0cfae94d45a7ec7d8f1837f83841ce26eb3
keshew <artyom.korotckov2017@yandex.ru>
2025-11-05 17:16:33 +0500

    Initial Commit

---
+++ Hen Hub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hen Hub
+//
+//  Created by Артём Коротков on 05.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hen Hub/Hen_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hen_HubApp.swift
+//  Hen Hub
+//
+//  Created by Артём Коротков on 05.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Hen_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
